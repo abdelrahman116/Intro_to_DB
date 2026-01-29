2a550689e5e459867dc10d5dccfc414914a025b2
عبدالرحمن أحمد السيد داود <112118879+abdelrahman116@users.noreply.github.com>
2026-01-29 17:16:02 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -21,27 +21,28 @@ CREATE TABLE books (
     publication_date DATE,
     CONSTRAINT fk_books_author
         FOREIGN KEY (author_id)
-        REFERENCES authors(author_id)
+        REFERENCES Authors(author_id)
 );
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     CONSTRAINT fk_orders_customer
         FOREIGN KEY (customer_id)
-        REFERENCES customers(customer_id)
+        REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
     CONSTRAINT fk_orderdetails_order
         FOREIGN KEY (order_id)
-        REFERENCES orders(order_id),
+        REFERENCES Orders(order_id),
     CONSTRAINT fk_orderdetails_book
         FOREIGN KEY (book_id)
-        REFERENCES books(book_id)
+        REFERENCES Books(book_id)
 );
+
