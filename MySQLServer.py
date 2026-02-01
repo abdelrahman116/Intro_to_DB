d461787a4ae841f67bff7b83c41a055af440fd30
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2026-02-01 17:21:31 +0200

    Update

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,1 +1,13 @@
-Hello
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="alx_book_store"
+)
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
+
+print(mydb.get_server_info())
