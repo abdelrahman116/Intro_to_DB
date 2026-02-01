c55bf34344fd40848d7c37f0accb3094a25bb6b1
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2026-02-01 18:29:18 +0200

    Update

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,3 @@
 USE mysql
 INSERT INTO customers(customer_id , customer_name ,email , address)
-VALUES (2,3,4), (`Blessing Malik`,`Obed Ehoneah`,`Nehemial Kamolu`), (`bmalik@sandtech.com`,`eobed@sandtech.com`,`nkamolu@sandtech.com`), ("124 Happiness  Ave.","125 Happiness Ave.","126 Happiness Ave.")
+VALUES (2,3,4), (`Blessing Malik`,`Obed Ehoneah`,`Nehemial Kamolu`), (`bmalik@sandtech.com`,`eobed@sandtech.com`,`nkamolu@sandtech.com`), ("124 Happiness  Ave.","125 Happiness  Ave.","126 Happiness  Ave.")
